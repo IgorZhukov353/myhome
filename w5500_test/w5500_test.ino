4d6effa85308ee2057bf3b56d047586b20fd3668
IgorZhukov353 <igorjukov353@ya.ru>
2020-06-21 20:40:01 +0300

    inet

--- w5500_test/w5500_test.ino
+++ w5500_test/w5500_test.ino
@@ -1,6 +1,15 @@
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet2.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp2.h>
+#include <Twitter.h>
+#include <util.h>
+
 #include <SPI.h>
 
-#include <Ethernet.h>
+//#include <Ethernet.h>
 
 // МАК адрес
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
@@ -24,108 +33,6 @@ int i = 0, count_read = 0;
   #define _mega "Setup start: not MEGA"
   #endif
 
-//------------------------------------------------------------------------
-// выполнить команду на удаленном сервере (через wi-fi)
-bool _send2site(String reqStr, String postBuf) 
-{
-
-//  String cmd1 = "AT+CIPSTART=\"TCP\",\"" + String(HOST_STR) +"\",80";
-  String request = (postBuf == "")? 
-    "GET /"  + reqStr + " HTTP/1.1\r\nHost: " + String(HOST_STR) + "\r\nConnection: close\r\n" :
-    "POST /" + reqStr + " HTTP/1.1\r\nHost: " + String(HOST_STR) + "\r\nConnection: close\r\nContent-Type: application/x-www-form-urlencoded\r\nAuthorization: Basic aWdvcmp1a292MzUzOlJEQ3RndjE5Ng==\r\nCache-Control: no-cache\r\nContent-Length: "  + postBuf.length() + "\r\n\r\n" + postBuf + "\r\n";
-
-  short requestLength = request.length() + 2; // add 2 because \r\n will be appended by Serial.println().
-  String cmd2 = "AT+CIPSEND=" + String(requestLength);
-
-  bool r;
-  for(short i=0; i<3; i++){
-      r = client.open(HOST_STR,80);  // установить соединение с хостом (3 попытки)
-      if(!r){
-        delay(1000);
-        continue;
-      }
-      r = espSendCommand( cmd2 , ok_str , 5000 );             // подготовить отсылку запроса - длина запроса
-      bytesSended += requestLength;
-      r = espSendCommand( request , (char*)"CLOSED" , 15000 );  // отослать запрос и получить ответ
-//      r = espSendCommand("AT+CIPCLOSE", ok_str , 5000 );
-      break;
-  }
-  if(!r){
-    sendErrorCounter++;   // счетчик ошибочных отправок (для определения проблемы доступа к интернету - возможно нужно перезагрузить роутер)
-    sendErrorCounter_ForAll++;
-  }
-  else
-    sendErrorCounter = 0;
-
-  sendCounter_ForAll++;  
-  lastWIFISended = millis();
-  return r;
-}
-
-//------------------------------------------------------------------------
-bool espSerialSetup() 
-{
-  bool r = true;
-  return r;
-}
-
-//---------------------------------------------------------------------
-bool espSendCommand(String cmd, char* goodResponse, unsigned long timeout) 
-{
-trace("espSendCommand( " + cmd + " , " + goodResponse + " , " + String(timeout) + " )" );
-ESP_Serial.println(cmd);
-unsigned long tnow, tstart;
-tnow = tstart = millis();
-String response;
-response.reserve(255);
-char c, cbuffer[11];
-short len = strlen(goodResponse);
-if( len > sizeof(cbuffer) - 1)
-  len = sizeof(cbuffer) - 1;
-cbuffer[sizeof(cbuffer) - 1] = 0;
-
-while( true ) {
-  if( tnow > tstart + timeout ) {
-    trace("espSendCommand: FAILED - Timeout exceeded " + String(timeout) + " seconds" );
-    if( response.length() > 0 ) {
-      trace("espSendCommand: RESPONSE:" + response);
-    } 
-    else {
-      trace("espSendCommand: NO RESPONSE");
-    }
-    return false;
-  } 
-  c = ESP_Serial.read();
-  if( c >= 0 ) {
-    response += String(c);
-    for(short i=0; i<sizeof(cbuffer)-1; i++)
-      cbuffer[i] = cbuffer[i+1];
-    cbuffer[sizeof(cbuffer) - 2] = c;
-
-    if(!memcmp(cbuffer + ((sizeof(cbuffer) - 1) - len),goodResponse,len)){
-      trace("espSendCommand: SUCCESS - Response time: " + String(millis() - tstart) + "ms.");
-      while(ESP_Serial.available()){
-          c = ESP_Serial.read();
-          response += String(c);
-        }
-      trace("RESPONSE: " + response + "\n\r---END RESPONSE---");
-      responseProcessing(response);
-      return true;
-      }
-    else
-      if(!memcmp(cbuffer + 5,"ERROR",5)){
-        trace("espSendCommand: ERROR - Response time: " + String(millis() - tstart) + "ms.");
-        while(ESP_Serial.available()){
-            c = ESP_Serial.read();
-            response += String(c);
-          }
-        trace("RESPONSE: " + response + "\n\r---END RESPONSE---");
-        return false;
-      }  
-    }
-    tnow = millis();
-  }
-}
 
 void setup() 
 {
@@ -145,7 +52,7 @@ void setup()
 
 void loop() 
 {
-  //return;
+
   if (client.connect(server, 80))
   {
     Serial.println("Есть подключение к серверу");
