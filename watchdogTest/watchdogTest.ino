59f7d157e0b73229cf964acd1ac88596383d1baf
IgorZhukov353 <igorJukov353@ya.ru>
2024-07-29 13:28:03 +0300

    1

--- watchdogTest/watchdogTest.ino
+++ watchdogTest/watchdogTest.ino
@@ -1,4 +1,21 @@
 #include <avr/wdt.h>
+//------------------------------------------------------------------------
+// Переменные, создаваемые процессом сборки,
+// когда компилируется скетч
+extern int __bss_end;
+extern void *__brkval;
+
+// Функция, возвращающая количество свободного ОЗУ (RAM)
+int checkMemoryFree() {
+  int freeValue;
+  if ((int)__brkval == 0)
+    freeValue = ((int)&freeValue) - ((int)&__bss_end);
+  else
+    freeValue = ((int)&freeValue) - ((int)__brkval);
+ // if(ramMemory > freeValue)
+   // ramMemory = freeValue;
+  return freeValue;
+}
 
 void setup() {
   wdt_disable(); // бесполезная строка до которой не доходит выполнение при bootloop
@@ -12,6 +29,7 @@ void setup() {
 }
 
 int timer = 0;
+int size = 1000;
 
 void loop(){
   // Каждую секунду мигаем светодиодом и значение счетчика пишем в Serial
@@ -19,6 +37,14 @@ void loop(){
     timer++;
     Serial.println(timer);
     digitalWrite(13, digitalRead(13)==1?0:1); delay(1);
+    Serial.println("Mem=" + String(checkMemoryFree()));
+    
+    void *ptr = malloc(size);
+    if(!ptr){
+      Serial.println("malloc" +String(size)+ " failed!");
+      size /= 2;
+    }
   }
-//  wdt_reset();
+  
+  wdt_reset();
 }
